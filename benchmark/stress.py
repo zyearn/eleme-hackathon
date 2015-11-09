4d2dcb77a88f178cbd7faf19ad1eb611fab3c11c
Lixin Yu <i@lxyu.net>
2015-11-10 00:00:00 +0800

    update stress test script

--- benchmark/stress.py
+++ benchmark/stress.py
@@ -1,18 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from __future__ import print_function
 
 import argparse
+import collections
 import contextlib
-import json
-import random
-import time
-import pymysql
-import os
-import redis
 import functools
-import math
-import traceback
 import itertools
+import json
+import math
+import os
+import random
 import signal
+import time
+
+import redis
+import pymysql
+
 
 try:
     import httplib
@@ -28,17 +33,23 @@ from multiprocessing.pool import Pool, ThreadPool
 from multiprocessing import Process
 
 KEY_PREFIX = "stress_test:make_order"
-FAILURE_KEY = "{}:failure".format(KEY_PREFIX)
-SUCCESS_KEY = "{}:success".format(KEY_PREFIX)
-RESPONSE_TIME_KEY = "{}:response_time".format(KEY_PREFIX)
-REQUEST_TIME_KEY = "{}:request_time".format(KEY_PREFIX)
-REQUEST_KEY = "{}:request".format(KEY_PREFIX)
-REQUEST_FAILD_KEY = "{}:request:failed".format(KEY_PREFIX)
+
 USER_KEY = "{}:user".format(KEY_PREFIX)
-ORDER_EVERY_SEC_KEY = "{}:orders_every_sec".format(KEY_PREFIX)
+
+SUCCESS_KEY = "{}:success".format(KEY_PREFIX)
+FAILURE_KEY = "{}:failure".format(KEY_PREFIX)
+
+ORDER_RESP_TIME_KEY = "{}:order_resp_time".format(KEY_PREFIX)
+REQ_RESP_TIME_KEY = "{}:req_resp_time".format(KEY_PREFIX)
+
+REQUEST_SUCCESS_KEY = "{}:request_success".format(KEY_PREFIX)
+REQUEST_FAILURE_KEY = "{}:request_failure".format(KEY_PREFIX)
+
+REQ_FINISH_TIME_KEY = "{}:req_finish_time".format(KEY_PREFIX)
+ORDER_FINISH_TIME_KEY = "{}:order_finish_time".format(KEY_PREFIX)
 
 redis_store = redis.Redis()
-users = foods = None
+users, foods = {}, []
 
 
 @contextlib.contextmanager
@@ -55,7 +66,7 @@ def db_query():
 
 
 def load_users():
-    users = {}
+    global users
     with db_query() as db:
         cur = db.cursor()
 
@@ -69,7 +80,7 @@ def load_users():
 
 
 def load_foods():
-    foods = []
+    global foods
     with db_query() as db:
         cur = db.cursor()
         cur.execute("SELECT id, stock, price FROM food")
@@ -82,11 +93,12 @@ def load_foods():
 def safe_loads(data):
     try:
         return json.loads(data)
-    except Exception:
+    except:
         return data
 
 
 class QueryException(Exception):
+
     def __init__(self, code, message):
         self.code = code
         self.message = message
@@ -96,12 +108,15 @@ class QueryException(Exception):
 
 
 class Query(object):
+
+    __slots__ = ["access_token", "user_id", "cart_id", "client"]
+
     def __init__(self, host, port):
+        self.client = httplib.HTTPConnection(host, port, timeout=3)
+
         self.access_token = None
         self.user_id = None
         self.cart_id = None
-
-        self.client = httplib.HTTPConnection(host, port, timeout=3)
 
     def request(self, method, url, headers=None, data=None):
         data = data or {}
@@ -113,15 +128,20 @@ class Query(object):
                             headers=headers)
         response = self.client.getresponse()
         status = response.status
-        data = response.read().decode("ascii")
+        data = response.read().decode("utf-8")
         self.client.close()
-        elapsed = time.time() - start
 
-        if status in (200, 204):
-            redis_store.incr(REQUEST_KEY)
-            redis_store.lpush(REQUEST_TIME_KEY, elapsed)
-        else:
-            redis_store.incr(REQUEST_FAILD_KEY)
+        now = time.time()
+        elapsed = now - start
+
+        with redis_store.pipeline() as p:
+            if status in (200, 204):
+                p.incr(REQUEST_SUCCESS_KEY)
+                p.lpush(REQ_FINISH_TIME_KEY, now)
+            else:
+                p.incr(REQUEST_FAILURE_KEY)
+            p.lpush(REQ_RESP_TIME_KEY, elapsed)
+            p.execute()
         return {"status": status, "data": safe_loads(data)}
 
     def url(self, path):
@@ -143,6 +163,9 @@ class Query(object):
 
     def login(self):
         user_id = redis_store.spop(USER_KEY)
+        if not user_id:
+            return False
+
         self.user_id = int(user_id)
         user = users[self.user_id]
         return self._do_login(user["username"], user["password"])
@@ -159,7 +182,7 @@ class Query(object):
         response = self.request("POST", self.url("/carts"))
         try:
             self.cart_id = response["data"].get("cart_id")
-        except Exception:
+        except:
             return False
         return response["status"] == 200
 
@@ -171,60 +194,56 @@ class Query(object):
         return res["status"] == 204
 
     def make_order(self):
-        for func in (self.login, self.get_foods, self.create_cart):
-            if func():
-                continue
-            return False
-
-        for i in range(random.randint(1, 3)):
-            if not self.cart_add_food():
+        chain = [self.login, self.get_foods, self.create_cart,
+                 self.cart_add_food, self.cart_add_food]
+        for action in chain:
+            if not action():
                 return False
 
         data = {"cart_id": self.cart_id}
         res = self.request("POST", self.url("/orders"), data=data)
-        if res["status"] == 200:
-            return True
-        return False
+        return res["status"] == 200
 
 
 def job(host, port):
     q = Query(host, port)
+
     start = time.time()
     try:
         ok = q.make_order()
     except:
-        if q.user_id:
-            redis_store.sadd(USER_KEY, q.user_id)
-        traceback.print_exc()
-        redis_store.incr(FAILURE_KEY)
-        return
-    elapsed = time.time() - start
+        ok = False
 
-    if ok:
-        redis_store.incr(SUCCESS_KEY)
-        redis_store.lpush(RESPONSE_TIME_KEY, elapsed)
-    else:
-        if q.user_id:
-            redis_store.sadd(USER_KEY, q.user_id)
-        redis_store.incr(FAILURE_KEY)
+    end = time.time()
+    elapsed = end - start
+
+    with redis_store.pipeline() as p:
+        if ok:
+            p.incr(SUCCESS_KEY)
+            p.lpush(ORDER_FINISH_TIME_KEY, end)
+        else:
+            p.incr(FAILURE_KEY)
+        p.lpush(ORDER_RESP_TIME_KEY, elapsed)
+        p.execute()
 
 
 def progress():
-    prev = cur = 0
-    delta = None
     try:
+        prev = 0
         while True:
-            start = time.time()
-            if delta is not None:
-                redis_store.lpush(ORDER_EVERY_SEC_KEY, delta)
             time.sleep(1)
-            elapsed = time.time() - start
             cur = get_value(SUCCESS_KEY)
-            delta = int((cur - prev) / elapsed)
+
+            msg = "Orders Per Second: {:4d}/s".format(cur - prev)
+            print(msg, end='')
+            print('\r' * len(msg), end='')
+
             prev = cur
-            print("Finished orders:", delta)
+
     except KeyboardInterrupt:
         pass
+    finally:
+        print('\n')
 
 
 def thread(host, port, threads, num):
@@ -252,28 +271,29 @@ def divide(n, m):
 def work(host, port, processes, threads, times):
     pool = Pool(processes,
                 lambda: signal.signal(signal.SIGINT, signal.SIG_IGN))
+    p = Process(target=progress)
+    p.daemon = True
 
     start = time.time()
+
     try:
         for chunk in divide(times, processes):
             pool.apply_async(thread, (host, port, threads, chunk))
+
+        p.start()
+
         pool.close()
-
-        t = Process(target=progress)
-        t.daemon = True
-        t.start()
-
         pool.join()
-        t.terminate()
-        t.join()
+        p.terminate()
+        p.join()
+
     except KeyboardInterrupt:
         pool.terminate()
-        t.terminate()
-        t.join()
+        p.terminate()
+        p.join()
         pool.join()
-    elapsed = time.time() - start
 
-    return elapsed
+    return time.time() - start
 
 
 def get_value(key):
@@ -290,50 +310,67 @@ def safe_div(a, b):
     return a / b if b else 0
 
 
-def report(processes, elapsed):
+def get_avg(l):
+    return safe_div(sum(l), float(len(l)))
+
+
+def report(processes, threads, total_time, total_order):
     success = get_value(SUCCESS_KEY)
     failure = get_value(FAILURE_KEY)
-    requests = get_value(REQUEST_KEY)
-    req_failed = get_value(REQUEST_FAILD_KEY)
+    req_success = get_value(REQUEST_SUCCESS_KEY)
+    req_failure = get_value(REQUEST_FAILURE_KEY)
 
-    response_time = get_range(RESPONSE_TIME_KEY)
-    request_time = get_range(REQUEST_TIME_KEY)
-    order_sec = get_range(ORDER_EVERY_SEC_KEY)
+    req_resp_time = get_range(REQ_RESP_TIME_KEY)
+    order_resp_time = get_range(ORDER_RESP_TIME_KEY)
+    req_finish_time = get_range(REQ_FINISH_TIME_KEY)
+    order_finish_time = get_range(ORDER_FINISH_TIME_KEY)
 
-    assert len(response_time) == success
-    assert len(request_time) == requests
+    assert len(order_resp_time) == success + failure
+    assert len(req_resp_time) == req_success + req_failure
 
-    total_time = float(sum(response_time))
-    avg = safe_div(total_time, success)
-    req_avg = safe_div(sum(request_time), float(requests))
-    ops = safe_div(success, elapsed)
-    qps = safe_div(requests, elapsed)
-    mean_order = safe_div(sum(order_sec), len(order_sec))
+    req_avg = safe_div(sum(req_resp_time), float(req_success))
+    order_avg = safe_div(sum(order_resp_time), success)
+
+    req_sec = collections.Counter(int(t) for t in req_finish_time)
+    order_sec = collections.Counter(int(t) for t in order_finish_time)
+
+    # remove the highest and lowest score
+    stats_req_sec = sorted(req_sec.values())[1:-1]
+    max_req_sec = int(get_avg(stats_req_sec[-5:]))
+    min_req_sec = int(get_avg(stats_req_sec[:5]))
+    mean_req_sec = int(get_avg(stats_req_sec))
+
+    # remove the highest and lowest score
+    stats_order_sec = sorted(order_sec.values())[1:-1]
+    max_order_sec = int(get_avg(stats_order_sec[-5:]))
+    min_order_sec = int(get_avg(stats_order_sec[:5]))
+    mean_order_sec = int(get_avg(stats_order_sec))
 
     p = functools.partial(print, sep='')
 
+    p("Score:                ", max_order_sec)
+    p("Correct Rate:         ", round(success / total_order * 100, 2), "%")
+
     p("\nStats")
-    p("Concurrenty Level:       ", processes)
-    p("Time taken for tests:    ", round(elapsed * 1000, 2), "ms")
-    p("Complete requests:       ", requests)
-    p("Failed requests:         ", req_failed)
-    p("Complete orders:         ", success)
-    p("Failed orders:           ", failure)
-    p("Time per request:        ", round(req_avg * 1000, 2), "ms", " (mean)")
-    p("Time per order:          ", round(avg * 1000, 2), "ms", " (mean)")
-    p("Request per second:      ", round(qps, 2))
-    p("Order per second:        ", round(ops, 2))
-    p("Orders for every second: ", int(max(order_sec)), " (max) ",
-      int(min(order_sec)), " (min) ", int(mean_order), " (mean)")
-    p()
-    p("Percentage of orders made within a certain time (ms)")
-    response_time = sorted(set(response_time)) if response_time else [0]
-    l = len(response_time)
+    p("Concurrent Level:     ", processes, " x ", threads)
+    p("Time taken for tests: ", round(total_time * 1000, 2), "ms")
+    p("Complete requests:    ", req_success)
+    p("Failed requests:      ", req_failure)
+    p("Complete orders:      ", success)
+    p("Failed orders:        ", failure)
+    p("Time per request:     ", round(req_avg * 1000, 2), "ms", " (mean)")
+    p("Time per order:       ", round(order_avg * 1000, 2), "ms", " (mean)")
+    p("Request per second:   ", max_req_sec, " (max) ", min_req_sec, " (min) ", mean_req_sec, " (mean)")  # noqa
+    p("Order per second:     ", max_order_sec, " (max) ", min_order_sec, " (min) ", mean_order_sec, " (mean)")  # noqa
+
+    p("\nPercentage of orders made within a certain time (ms)")
+    order_resp_time = sorted(set(order_resp_time)) if order_resp_time else [0]
+    l = len(order_resp_time)
     for e in (0.5, 0.75, 0.8, 0.9, 0.95, 0.98, 1):
         idx = int(l * e)
         idx = 0 if idx == 0 else idx - 1
         p(" {:>4.0%}      ".format(e),
-          int(math.ceil(response_time[idx] * 1000)))
+          int(math.ceil(order_resp_time[idx] * 1000)))
 
 
 def main():
@@ -348,18 +385,22 @@ def main():
                         help="threads")
     parser.add_argument("-n", "--num", default=10000, type=int,
                         help="requests")
+
     args = parser.parse_args()
 
-    redis_store.delete(SUCCESS_KEY, FAILURE_KEY, RESPONSE_TIME_KEY,
-                       REQUEST_FAILD_KEY, REQUEST_KEY, USER_KEY,
-                       REQUEST_TIME_KEY, ORDER_EVERY_SEC_KEY)
+    redis_store.delete(
+        USER_KEY, SUCCESS_KEY, FAILURE_KEY,
+        ORDER_RESP_TIME_KEY, REQ_RESP_TIME_KEY,
+        REQUEST_SUCCESS_KEY, REQUEST_FAILURE_KEY,
+        REQ_FINISH_TIME_KEY, ORDER_FINISH_TIME_KEY)
 
-    global users, foods
-    users, foods = load_users(), load_foods()
+    load_users()
+    load_foods()
 
-    elapsed = work(args.host, args.port, args.processes, args.threads,
-                   args.num)
-    report(args.processes, elapsed)
+    total_time = work(
+        args.host, args.port, args.processes, args.threads, args.num)
+
+    report(args.processes, args.threads, total_time, args.num)
 
 
 if __name__ == "__main__":
