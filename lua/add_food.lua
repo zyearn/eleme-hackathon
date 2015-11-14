f258e1073040010f7ca829325e80815e89dc0d66
Vic Luo <vicluo96@gmail.com>
2015-11-14 14:31:31 +0800

    Implement lua script for add_food
    Signed-off-by: Vic Luo <vicluo96@gmail.com>

---
+++ lua/add_food.lua
@@ -0,0 +1,22 @@
+local belong_user = redis.call('get', 'token:'..KEYS[1]..':user'); 
+local cart_user = redis.call('get', 'cart:'..KEYS[2]..':user');
+if not cart_user then
+        return -1  end;                                                          
+if cart_user == belong_user then
+        if redis.call('hexists', 'food:stock', KEYS[3]) then
+                local origin = redis.call('hgetall', 'cart:'..KEYS[2]);
+                local sum = 0;
+                for i = 2,#origin, 2 do
+                        sum = sum + origin[i]
+                end
+                if sum + KEYS[4] > 3 then
+                        return -3;
+                end
+                redis.call('hincrby', 'cart:'..KEYS[2], KEYS[3], KEYS[4])
+                return 0
+        else
+                return -2
+        end
+else
+        return -4
+end
